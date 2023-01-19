83e079ee06d8e865b6564792d81b25959b84722e
Jadan Morrow <jadanmorrow@outlook.com>
2023-01-18 21:40:18 -0700

    update

--- dnslookup.ps1
+++ dnslookup.ps1
@@ -7,11 +7,25 @@ $dnsServers = "8.8.8.8","8.8.4.4","208.67.222.222","208.67.220.220"
 
 foreach($dnsServer in $dnsServers){
     $nsRecords = (Resolve-DnsName -Type NS -Name $domain -Server $dnsServer)
-    if ($nsRecords) {
+    $aRecords = (Resolve-DnsName -Type A -Name $domain -Server $dnsServer)
+    $mxRecords = (Resolve-DnsName -Type MX -Name $domain -Server $dnsServer)
+    if ($nsRecords -or $aRecords -or $mxRecords) {
         Write-Host "*****************************************************************************************************"
         Write-Host "DNS Server Used: $dnsServer"
         Write-Host "ANSWER SECTION:"
-        $nsRecords.NameHost | % { Write-Host "    $($_)" }
+        if ($nsRecords) {
+            Write-Host "    NS Records:"
+            $nsRecords.NameHost | % { Write-Host "        $($_)" }
+        }
+        if ($aRecords) {
+            Write-Host "    A Records:"
+            $aRecords.IPAddress | % { Write-Host "        $($_)" }
+        }
+        if ($mxRecords) {
+            Write-Host "    MX Records:"
+            $mxRecords.MailExchange | % { Write-Host "        $($_)" }
+        }
         Write-Host "Timestamp: $(Get-Date)"
+        Write-Host "Query time: $($nsRecords.QueryTime) ms"
     }
 }
