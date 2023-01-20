5d2356d6da9c9f28d6f0b08e36f219f3cd63ea0f
Jadan Morrow <jadanmorrow@outlook.com>
2023-01-20 15:36:01 -0700

    update

--- dnslookup.ps1
+++ dnslookup.ps1
@@ -3,29 +3,33 @@ param(
 )
 
 # DNS servers to query
-$dnsServers = "8.8.8.8","8.8.4.4","208.67.222.222","208.67.220.220"
+$dnsServers = "8.8.8.8","8.8.4.4","208.67.222.222","208.67.220.220","8.20.247.10","8.20.247.11","50.87.144.163"
 
 foreach($dnsServer in $dnsServers){
-    $nsRecords = (Resolve-DnsName -Type NS -Name $domain -Server $dnsServer)
-    $aRecords = (Resolve-DnsName -Type A -Name $domain -Server $dnsServer)
-    $mxRecords = (Resolve-DnsName -Type MX -Name $domain -Server $dnsServer)
-    if ($nsRecords -or $aRecords -or $mxRecords) {
-        Write-Host "*****************************************************************************************************"
-        Write-Host "DNS Server Used: $dnsServer"
-        Write-Host "ANSWER SECTION:"
-        if ($nsRecords) {
-            Write-Host "    NS Records:"
-            $nsRecords.NameHost | % { Write-Host "        $($_)" }
+    try {
+        $nsRecords = (Resolve-DnsName -Type NS -Name $domain -Server $dnsServer)
+        $aRecords = (Resolve-DnsName -Type A -Name $domain -Server $dnsServer)
+        $mxRecords = (Resolve-DnsName -Type MX -Name $domain -Server $dnsServer)
+        if ($nsRecords -or $aRecords -or $mxRecords) {
+            Write-Host "*****************************************************************************************************"
+            Write-Host "DNS Server Used: $dnsServer"
+            Write-Host "ANSWER SECTION:"
+            if ($nsRecords) {
+                Write-Host "    NS Records:"
+                $nsRecords.NameHost | % { Write-Host "        $($_)" }
+            }
+            if ($aRecords) {
+                Write-Host "    A Records:"
+                $aRecords.IPAddress | % { Write-Host "        $($_)" }
+            }
+            if ($mxRecords) {
+                Write-Host "    MX Records:"
+                $mxRecords.MailExchange | % { Write-Host "        $($_)" }
+            }
+            Write-Host "Timestamp: $(Get-Date)"
+            Write-Host "Query time: $($nsRecords.QueryTime) ms"
         }
-        if ($aRecords) {
-            Write-Host "    A Records:"
-            $aRecords.IPAddress | % { Write-Host "        $($_)" }
-        }
-        if ($mxRecords) {
-            Write-Host "    MX Records:"
-            $mxRecords.MailExchange | % { Write-Host "        $($_)" }
-        }
-        Write-Host "Timestamp: $(Get-Date)"
-        Write-Host "Query time: $($nsRecords.QueryTime) ms"
+    } catch {
+        Write-Host "An error occurred: $($_.Exception.Message)"
     }
 }
